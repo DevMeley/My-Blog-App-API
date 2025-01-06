06cbe8e6ac133f4ef0a64d03802a1022dc53e32a
DevMeley <ayantolaAbisola@outlook.com>
2025-01-06 13:35:14 +0100

    second commit on monday 6th

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(cors({
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images"))); 
 
 
 
