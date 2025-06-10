d95badd55c2c7208570c49f9ba5e29b21ce5da5c
Ayantola Abisola Monsurat <ayantolamonsurat@gmail.com>
2025-06-10 16:17:46 +0100

    I don't know what happened

--- db.js
+++ db.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose")
 
 const connectDB = async () => {
+    // try {
+    //     const connection = await mongoose.connect('mongodb://root:example@localhost:27017/?authSource=admin')
+    // console.log(`Database connected successfully to ${connection.connection.host}`)
+    // } catch (error) {
+    //     console.log(error)
+    // }
     try {
-        const connection = await mongoose.connect('mongodb://root:example@localhost:27017/?authSource=admin')
+        const connection = await mongoose.connect('mongodb+srv://DevMeley:DevMeley@blog-app-db.bxgxcz7.mongodb.net/?retryWrites=true&w=majority&appName=Blog-App-DB')
     console.log(`Database connected successfully to ${connection.connection.host}`)
     } catch (error) {
         console.log(error)
