c11b6400048eaf70841a031ef19cd61a86e04c87
Ayantola Abisola Monsurat <ayantolamonsurat@gmail.com>
2025-06-15 14:27:44 +0100

    Add production URL to CORS origin settings

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express()
 
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:2300', 'http://127.0.0.1:2300'],
+    origin: ['http://localhost:5173', 'http://localhost:2300', 'http://127.0.0.1:2300', "https://blog-app-with-react-js-rho.vercel.app"],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }));
