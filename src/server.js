6cdda11fe18a9c073cc9169380066cbf9bc4f342
Ayantola Abisola Monsurat <ayantolamonsurat@gmail.com>
2025-06-15 14:08:47 +0100

    Set server port to a fixed value of 2300

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv")
 
 dotenv.config()
 
-const PORT = process.env.PORT || 2300
+const PORT = 2300
 
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`)
