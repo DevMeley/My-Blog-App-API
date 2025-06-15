bd0be05bec4937c0628fa33abde37be2da59378a
Ayantola Abisola Monsurat <ayantolamonsurat@gmail.com>
2025-06-15 14:11:11 +0100

    Uncomment config.js and restore its content for configuration settings

---
+++ config/config.js
@@ -0,0 +1,17 @@
+// // config.js
+
+// const config = {
+//   app: {
+//     port: 2300,
+//     environment: "development",
+//   },
+//   database: {
+//     host: "localhost",
+//     user: "root",
+//     password: "example",
+//     name: "blogAppDB",
+//   },
+//   jwt_secret_code: "ayokunnu",
+// };
+
+// module.exports = config; // Export the config object
