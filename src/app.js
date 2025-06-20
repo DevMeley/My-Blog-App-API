d660b456945136c835233dafee99468517311e26
Ayantola Abisola Monsurat <ayantolamonsurat@gmail.com>
2025-06-20 18:13:12 +0100

    Reorder middleware to ensure JSON parsing occurs before URL-encoded parsing

--- src/app.js
+++ src/app.js
@@ -20,8 +20,8 @@ app.use(cors({
 
   
 
-app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
 
 
